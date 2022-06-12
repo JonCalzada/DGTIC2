f06f24be6e1a9b47f29c726bab195dcedcb0fe3c
Jonathan Calzada <jcalzadamtz@gmail.com>
2022-06-12 02:31:38 -0500

    No mover con Imagen, sin estilos

--- proDGTIC/resources/views/welcome.blade.php
+++ proDGTIC/resources/views/welcome.blade.php
@@ -1,9 +1,14 @@
 @extends('theme.base')
 
+
 @section('content')
 
+    <img src="https://educacioncontinua.unam.mx/uploads_img/DGTIC.png">
+    <img src="/proDGTIC/resources/views/becas.png" width="180" height="150" text-align: right;>
     <div class="container py-5 text-center">
-        <h1 class="">Hola mundo</h1>
+        <h1 class="">Sistema en Laravel  Programa de Becarios de Ingenieria en Software</h1>
+        <h2 class="">Simulacion de una veterinaria</h2>
         <a href="{{ route('client.index') }}" class="btn btn-primary">Clientes</a>
+        
     </div>
 @endsection
